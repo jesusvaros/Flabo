9f9d23c028ea767566633210473e780be86aa596
jesusvaros <xjesusvr@gmail.com>
2025-01-20 19:45:31 +0100

    Create ticket modal

--- src/app/components/TabContents/TicketsTab.tsx
+++ src/app/components/TabContents/TicketsTab.tsx
@@ -2,6 +2,7 @@ import { TicketCard } from "../Cards/TicketCard";
 import { createClient } from "../../../../utils/supabase/server";
 import { Grid } from "./CollectionsTab.styles";
 import { SuspenseTab } from "./SuspenseTab";
+import { CreateTicketCard } from "../Tickets/CreateTicketCard";
 
 export const TicktetsTabSuspense = () => {
   return (
@@ -24,7 +25,8 @@ export const TicketsTab = async () => {
 
   return (
     <Grid>
-      {tickets.map((ticket) => (
+      <CreateTicketCard />
+      {tickets?.map((ticket) => (
         <TicketCard key={ticket.id} ticket={ticket} />
       ))}
     </Grid>
